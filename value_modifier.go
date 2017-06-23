871e514c7ef72f6eb94dbf2d30aaaa196e1539d4
Tim Schindler <xh3b4sd@gmail.com>
2017-06-23 21:08:26 +0200

    added basic implementation of JSON/YAML traversing

---
+++ value_modifier.go
@@ -0,0 +1,92 @@
+package valuemodifier
+
+import (
+	"encoding/json"
+
+	microerror "github.com/giantswarm/microkit/error"
+	yaml "gopkg.in/yaml.v1"
+)
+
+func traverseJSON(input []byte, valueModifiers ...ValueModifier) ([]byte, error) {
+	var m map[string]interface{}
+	err := json.Unmarshal(input, &m)
+	if err != nil {
+		return nil, microerror.MaskAny(err)
+	}
+
+	for k, v := range m {
+		m[k] = toModifiedValueJSON(v, valueModifiers...)
+	}
+	b, err := json.MarshalIndent(m, "", "  ")
+	if err != nil {
+		return nil, microerror.MaskAny(err)
+	}
+
+	return b, nil
+}
+
+func traverseYAML(input []byte, valueModifiers ...ValueModifier) ([]byte, error) {
+	var m map[interface{}]interface{}
+	err := yaml.Unmarshal(input, &m)
+	if err != nil {
+		return nil, microerror.MaskAny(err)
+	}
+
+	for k, v := range m {
+		m[k] = toModifiedValueYAML(v, valueModifiers...)
+	}
+	b, err := yaml.Marshal(m)
+	if err != nil {
+		return nil, microerror.MaskAny(err)
+	}
+
+	return b, nil
+}
+
+func toModifiedValueJSON(val interface{}, valueModifiers ...ValueModifier) interface{} {
+	m, ok := val.(map[string]interface{})
+	if ok {
+		for k, v := range m {
+			m[k] = toModifiedValueJSON(v, valueModifiers...)
+		}
+
+		return m
+	}
+
+	s, ok := val.(string)
+	if ok {
+		for _, m := range valueModifiers {
+			o, err := m.Modify([]byte(s))
+			if err != nil {
+				panic(err)
+			}
+			s = string(o)
+		}
+	}
+
+	return s
+}
+
+func toModifiedValueYAML(val interface{}, valueModifiers ...ValueModifier) interface{} {
+	m, ok := val.(map[interface{}]interface{})
+	if ok {
+		for k, v := range m {
+			m[k] = toModifiedValueYAML(v, valueModifiers...)
+		}
+
+		return m
+	}
+
+	s, ok := val.(string)
+	if ok {
+		for _, m := range valueModifiers {
+			o, err := m.Modify([]byte(s))
+			if err != nil {
+				panic(err)
+			}
+			s = string(o)
+		}
+	}
+
+	return s
+}
---
+++ value_modifier_test.go
@@ -0,0 +1,100 @@
+package valuemodifier
+
+import (
+	"fmt"
+	"testing"
+)
+
+type testModifier struct{}
+
+func (m testModifier) Modify(value []byte) ([]byte, error) {
+	return []byte(string(value) + "-modified"), nil
+}
+
+func Test_ValueModifier_traverseJSON(t *testing.T) {
+	testJSON := `{
+  "block1": {
+    "block11": {
+      "pass1": "pass1"
+    },
+    "pass2": "pass2"
+  },
+  "block2": {
+    "block21": {
+      "pass3": "pass3"
+    },
+    "pass4": "pass4"
+  },
+  "pass5": "pass5",
+  "pass6": "pass6"
+}`
+
+	valueModifiers := []ValueModifier{
+		testModifier{},
+	}
+
+	output, err := traverseJSON([]byte(testJSON), valueModifiers...)
+	if err != nil {
+		t.Fatal("expected", nil, "got", err)
+	}
+
+	expectedJSON := `{
+  "block1": {
+    "block11": {
+      "pass1": "pass1-modified"
+    },
+    "pass2": "pass2-modified"
+  },
+  "block2": {
+    "block21": {
+      "pass3": "pass3-modified"
+    },
+    "pass4": "pass4-modified"
+  },
+  "pass5": "pass5-modified",
+  "pass6": "pass6-modified"
+}`
+
+	if string(output) != expectedJSON {
+		t.Fatal("expected", fmt.Sprintf("%q", expectedJSON), "got", fmt.Sprintf("%q", output))
+	}
+}
+
+func Test_ValueModifier_traverseYAML(t *testing.T) {
+	testYAML := `block1:
+  block11:
+    pass1: pass1
+  pass2: pass2
+block2:
+  block21:
+    pass3: pass3
+  pass4: pass4
+pass5: pass5
+pass6: pass6
+`
+
+	valueModifiers := []ValueModifier{
+		testModifier{},
+	}
+
+	output, err := traverseYAML([]byte(testYAML), valueModifiers...)
+	if err != nil {
+		t.Fatal("expected", nil, "got", err)
+	}
+
+	expectedYAML := `block1:
+  block11:
+    pass1: pass1-modified
+  pass2: pass2-modified
+block2:
+  block21:
+    pass3: pass3-modified
+  pass4: pass4-modified
+pass5: pass5-modified
+pass6: pass6-modified
+`
+
+	if string(output) != expectedYAML {
+		t.Fatal("expected", fmt.Sprintf("%q", expectedYAML), "got", fmt.Sprintf("%q", output))
+	}
+}
