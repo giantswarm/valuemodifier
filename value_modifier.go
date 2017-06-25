8a3c658c1420db8672cd74ec16ff5b3b0ebc875b
Tim Schindler <xh3b4sd@gmail.com>
2017-06-25 16:45:38 +0200

    added ignore fields

--- value_modifier.go
+++ value_modifier.go
@@ -8,7 +8,56 @@ import (
 	yaml "gopkg.in/yaml.v1"
 )
 
-func TraverseJSON(input []byte, valueModifiers ...ValueModifier) ([]byte, error) {
+// Config represents the configuration used to create a new value modifier
+// traverser.
+type Config struct {
+	// Dependencies.
+	ValueModifiers []ValueModifier
+
+	// Settings.
+	IgnoreFields []string
+}
+
+// DefaultConfig provides a default configuration to create a new GPG decryption
+// decoding value modifier by best effort.
+func DefaultConfig() Config {
+	return Config{
+		// Dependencies.
+		ValueModifiers: nil,
+
+		// Settings.
+		IgnoreFields: nil,
+	}
+}
+
+// New creates a new configured GPG decryption value modifier.
+func New(config Config) (*Service, error) {
+	// Dependencies.
+	if len(config.ValueModifiers) == 0 {
+		return nil, microerror.MaskAnyf(invalidConfigError, "config.ValueModifiers must not be empty")
+	}
+
+	newService := &Service{
+		// Dependencies.
+		valueModifiers: config.ValueModifiers,
+
+		// Settings.
+		ignoreFields: config.IgnoreFields,
+	}
+
+	return newService, nil
+}
+
+// Service implements the GPG decryption value modifier.
+type Service struct {
+	// Dependencies.
+	valueModifiers []ValueModifier
+
+	// Settings.
+	ignoreFields []string
+}
+
+func (s *Service) TraverseJSON(input []byte) ([]byte, error) {
 	var m map[string]interface{}
 	err := json.Unmarshal(input, &m)
 	if err != nil {
@@ -16,7 +65,7 @@ func TraverseJSON(input []byte, valueModifiers ...ValueModifier) ([]byte, error)
 	}
 
 	for k, v := range m {
-		m[k] = toModifiedValueJSON(v, valueModifiers...)
+		m[k] = toModifiedValueJSON(k, v, s.ignoreFields, s.valueModifiers...)
 	}
 	b, err := json.MarshalIndent(m, "", "  ")
 	if err != nil {
@@ -26,7 +75,7 @@ func TraverseJSON(input []byte, valueModifiers ...ValueModifier) ([]byte, error)
 	return b, nil
 }
 
-func TraverseYAML(input []byte, valueModifiers ...ValueModifier) ([]byte, error) {
+func (s *Service) TraverseYAML(input []byte) ([]byte, error) {
 	var m map[interface{}]interface{}
 	err := yaml.Unmarshal(input, &m)
 	if err != nil {
@@ -34,7 +83,7 @@ func TraverseYAML(input []byte, valueModifiers ...ValueModifier) ([]byte, error)
 	}
 
 	for k, v := range m {
-		m[k] = toModifiedValueYAML(v, valueModifiers...)
+		m[k] = toModifiedValueYAML(k, v, s.ignoreFields, s.valueModifiers...)
 	}
 	b, err := yaml.Marshal(m)
 	if err != nil {
@@ -44,11 +93,11 @@ func TraverseYAML(input []byte, valueModifiers ...ValueModifier) ([]byte, error)
 	return b, nil
 }
 
-func toModifiedValueJSON(val interface{}, valueModifiers ...ValueModifier) interface{} {
+func toModifiedValueJSON(key string, val interface{}, ignoreFields []string, valueModifiers ...ValueModifier) interface{} {
 	m, ok := val.(map[string]interface{})
 	if ok {
 		for k, v := range m {
-			m[k] = toModifiedValueJSON(v, valueModifiers...)
+			m[k] = toModifiedValueJSON(k, v, ignoreFields, valueModifiers...)
 		}
 
 		return m
@@ -56,6 +105,11 @@ func toModifiedValueJSON(val interface{}, valueModifiers ...ValueModifier) inter
 
 	s := cast.ToString(val)
 	if s != "" {
+		for _, f := range ignoreFields {
+			if f == key {
+				return s
+			}
+		}
 		for _, m := range valueModifiers {
 			o, err := m.Modify([]byte(s))
 			if err != nil {
@@ -68,11 +122,11 @@ func toModifiedValueJSON(val interface{}, valueModifiers ...ValueModifier) inter
 	return s
 }
 
-func toModifiedValueYAML(val interface{}, valueModifiers ...ValueModifier) interface{} {
+func toModifiedValueYAML(key interface{}, val interface{}, ignoreFields []string, valueModifiers ...ValueModifier) interface{} {
 	m, ok := val.(map[interface{}]interface{})
 	if ok {
 		for k, v := range m {
-			m[k] = toModifiedValueYAML(v, valueModifiers...)
+			m[k] = toModifiedValueYAML(k, v, ignoreFields, valueModifiers...)
 		}
 
 		return m
@@ -80,6 +134,11 @@ func toModifiedValueYAML(val interface{}, valueModifiers ...ValueModifier) inter
 
 	s := cast.ToString(val)
 	if s != "" {
+		for _, f := range ignoreFields {
+			if f == key {
+				return s
+			}
+		}
 		for _, m := range valueModifiers {
 			o, err := m.Modify([]byte(s))
 			if err != nil {
--- value_modifier_test.go
+++ value_modifier_test.go
@@ -12,7 +12,27 @@ func (m testModifier) Modify(value []byte) ([]byte, error) {
 }
 
 func Test_ValueModifier_TraverseJSON(t *testing.T) {
-	testJSON := `{
+	var err error
+	var newService *Service
+	{
+		config := DefaultConfig()
+		config.ValueModifiers = []ValueModifier{
+			testModifier{},
+		}
+		config.IgnoreFields = []string{
+			"noSecret1",
+			"noSecret2",
+		}
+		newService, err = New(config)
+		if err != nil {
+			t.Fatal("expected", nil, "got", err)
+		}
+	}
+
+	var expectedJSON string
+	var testJSON string
+	{
+		testJSON = `{
   "block1": {
     "block11": {
       "pass1": "pass1"
@@ -25,20 +45,12 @@ func Test_ValueModifier_TraverseJSON(t *testing.T) {
     },
     "pass4": "pass4"
   },
+  "noSecret1": "foo",
+  "noSecret2": "bar",
   "pass5": "pass5",
   "pass6": 123456
 }`
-
-	valueModifiers := []ValueModifier{
-		testModifier{},
-	}
-
-	output, err := TraverseJSON([]byte(testJSON), valueModifiers...)
-	if err != nil {
-		t.Fatal("expected", nil, "got", err)
-	}
-
-	expectedJSON := `{
+		expectedJSON = `{
   "block1": {
     "block11": {
       "pass1": "pass1-modified"
@@ -51,17 +63,46 @@ func Test_ValueModifier_TraverseJSON(t *testing.T) {
     },
     "pass4": "pass4-modified"
   },
+  "noSecret1": "foo",
+  "noSecret2": "bar",
   "pass5": "pass5-modified",
   "pass6": "123456-modified"
 }`
+	}
 
-	if string(output) != expectedJSON {
-		t.Fatal("expected", fmt.Sprintf("%q", expectedJSON), "got", fmt.Sprintf("%q", output))
+	{
+		output, err := newService.TraverseJSON([]byte(testJSON))
+		if err != nil {
+			t.Fatal("expected", nil, "got", err)
+		}
+		if string(output) != expectedJSON {
+			t.Fatal("expected", fmt.Sprintf("%q", expectedJSON), "got", fmt.Sprintf("%q", output))
+		}
 	}
 }
 
 func Test_ValueModifier_TraverseYAML(t *testing.T) {
-	testYAML := `block1:
+	var err error
+	var newService *Service
+	{
+		config := DefaultConfig()
+		config.ValueModifiers = []ValueModifier{
+			testModifier{},
+		}
+		config.IgnoreFields = []string{
+			"noSecret1",
+			"noSecret2",
+		}
+		newService, err = New(config)
+		if err != nil {
+			t.Fatal("expected", nil, "got", err)
+		}
+	}
+
+	var expectedYAML string
+	var testYAML string
+	{
+		testYAML = `block1:
   block11:
     pass1: pass1
   pass2: pass2
@@ -69,20 +110,12 @@ block2:
   block21:
     pass3: pass3
   pass4: pass4
+noSecret1: foo,
+noSecret2: bar,
 pass5: pass5
 pass6: 1234565
 `
-
-	valueModifiers := []ValueModifier{
-		testModifier{},
-	}
-
-	output, err := TraverseYAML([]byte(testYAML), valueModifiers...)
-	if err != nil {
-		t.Fatal("expected", nil, "got", err)
-	}
-
-	expectedYAML := `block1:
+		expectedYAML = `block1:
   block11:
     pass1: pass1-modified
   pass2: pass2-modified
@@ -90,11 +123,20 @@ block2:
   block21:
     pass3: pass3-modified
   pass4: pass4-modified
+noSecret1: foo,
+noSecret2: bar,
 pass5: pass5-modified
 pass6: 1234565-modified
 `
+	}
 
-	if string(output) != expectedYAML {
-		t.Fatal("expected", fmt.Sprintf("%q", expectedYAML), "got", fmt.Sprintf("%q", output))
+	{
+		output, err := newService.TraverseYAML([]byte(testYAML))
+		if err != nil {
+			t.Fatal("expected", nil, "got", err)
+		}
+		if string(output) != expectedYAML {
+			t.Fatal("expected", fmt.Sprintf("%q", expectedYAML), "got", fmt.Sprintf("%q", output))
+		}
 	}
 }
