19897035486cbe26262b07fe36e4ec97bd0aa25d
Tim Schindler <xh3b4sd@gmail.com>
2017-07-26 13:05:36 +0200

    added inspect value modifier

---
+++ inspect/error.go
@@ -0,0 +1,12 @@
+package inspect
+
+import (
+	"github.com/juju/errgo"
+)
+
+var invalidConfigError = errgo.New("invalid config")
+
+// IsInvalidConfig asserts invalidConfigError.
+func IsInvalidConfig(err error) bool {
+	return errgo.Cause(err) == invalidConfigError
+}
